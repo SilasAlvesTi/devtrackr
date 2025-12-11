a712da95eb789367c1033da522d22399a8c7647c
SilasAlvesTi <silas.alves.sa04@gmail.com>
2025-12-11 16:50:33 -0300

    finalizando implementação do core

---
+++ src/DevTrackR.ShippingOrders.Core/ValueObjects/DeliveryAddress.cs
@@ -0,0 +1,12 @@
+﻿namespace DevTrackR.ShippingOrders.Core.ValueObjects
+{
+    public record DeliveryAddress(
+        string Street,
+        string Number, 
+        string ZipCode, 
+        string City,
+        string State,
+        string Country)
+    {
+    }
+}
